68c8cebc41061515f00b707864922d5002d93ee8
Bilal <bilalrukundi1658@gmail.com>
2025-05-26 12:34:12 +0530

    Fixed The Linear Search Algorithem

--- LinearSearch.java
+++ LinearSearch.java
@@ -2,40 +2,28 @@ import java.util.Scanner;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
 
-            System.out.print("Enter the size of your array: ");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the Size Of Your Array: ");
             int size = sc.nextInt();
 
             int[] arr = new int[size];
 
-            System.out.println("Enter elements of the array:");
+            System.out.println("Enter elemnts of your array: ");
             for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            System.out.print("Enter the target element to search: ");
+            System.out.println("Enter A Target: ");
             int target = sc.nextInt();
 
-            int mid = (arr.length + (arr[arr.length-1]))/2;
-
-            // Linear search logic
-            boolean found = false;
             for (int i = 0; i < size; i++) {
-                if (arr[mid] == target) {
-                    System.out.println("Target found at index " + i);
-                    found = true;
-                    break;
-                } else if(arr[mid] < target){
-                    arr[i] = arr[mid + 1];
-                }else if(arr[mid] > target){
-                    arr[i] = arr[mid - 1];
+                if (arr[i] == target) {
+                    System.out.println("Index: " + i + " Element: " + arr[i]);
                 }
             }
 
-            if (!found) {
-                System.out.println("Target not found in the array.");
-            }
         }
+
     }
 }
