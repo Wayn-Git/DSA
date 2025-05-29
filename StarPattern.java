0a1c293f70b19d2ea7983077ea2660d4df79f632
Bilal <ejramwayne@gmail.com>
2025-05-29 16:03:10 +0530

    Update StarPattern.java

--- StarPattern.java
+++ StarPattern.java
@@ -1,12 +1,26 @@
 // Right angle Traingle
 
+// public class StarPattern {
+//     public static void main(String[] args){
+//         for(int i = 0; i < 6; i++ ){
+//             for(int j = 0; j < i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// } 
+
+
+// Inverted Traingle 
+
+
 public class StarPattern {
     public static void main(String[] args){
-        for(int i = 0; i < 6; i++ ){
-            for(int j = 0; j < i; j++){
+        for(int i = 5; i > 0; i++ ){
+            for(int j = 5; j > i; j--)
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-} 
