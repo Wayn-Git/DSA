6cdf890738a24b13fbb7882d02d1fa2ed0bc79f0
Bilal <ejramwayne@gmail.com>
2025-05-29 11:19:33 +0530

    LinearSearch Comments

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,25 +1,25 @@
-import java.util.Scanner;
+import java.util.Scanner; //Importing Scanner to get the user input 
 
 public class LinearSearch {
     public static void main(String[] args) {
 
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter the Size Of Your Array: ");
+            System.out.println("Enter the Size Of Your Array: ");  // Getting the size of the Array
             int size = sc.nextInt();
 
-            int[] arr = new int[size];
+            int[] arr = new int[size]; // Storing the size of the array 
 
-            System.out.println("Enter elemnts of your array: ");
+            System.out.println("Enter elemnts of your array: ");  //Getting the elements of the array 
             for (int i = 0; i < size; i++) {
                 arr[i] = sc.nextInt();
             }
 
-            System.out.println("Enter A Target: ");
+            System.out.println("Enter A Target: ");  //Getting the target element
             int target = sc.nextInt();
 
-            for (int i = 0; i < size; i++) {
-                if (arr[i] == target) {
-                    System.out.println("Index: " + i + " Element: " + arr[i]);
+            for (int i = 0; i < size; i++) {   // The loop iterates over each element of the array 
+                if (arr[i] == target) {   // The if statement checks weather the eleemnt of the array is = to the target 
+                    System.out.println("Index: " + i + " Element: " + arr[i]); // if true we return the index and the element
                 }
             }
 
