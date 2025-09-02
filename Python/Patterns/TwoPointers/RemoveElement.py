a0ae79837c32fc209a982f7a30b17ca0f25abaac
Bilal <bilalrukundi1658@gmail.com>
2025-09-02 21:58:32 +0530

    Documented RemoveElement Code

--- Python/Patterns/TwoPointers/RemoveElement.py
+++ Python/Patterns/TwoPointers/RemoveElement.py
@@ -1,7 +1,12 @@
-Arr = [-1,-2,-9,0,1,2,-3,-4]
-Sqr_Arr = [x**2 for x in Arr]
+# Question: Sqare the elements of array and sort them
+
+
+Arr = [-1,-2,-9,0,1,2,-3,-4] # An Array with random elements
+Sqr_Arr = [x**2 for x in Arr] # Squaring the elements of the array 
 print(Sqr_Arr)
 
+
+# Bubble Sort
 for i in range(len(Sqr_Arr)):
     for j in range(0,len(Sqr_Arr) - i - 1):
         if(Sqr_Arr[j] > Sqr_Arr[j+1]):
